dbf3a95640382c447155f7a85b83552fbc57afee
Larry Shimmell <larryandshimmell@gmail.com>
2018-07-16 10:29:17 -0400

    updated card flip function

--- match.js
+++ match.js
@@ -4,6 +4,9 @@ var Deck = ['Cards/2S.png2', 'Cards/3S.png3', 'Cards/4S.png4', 'Cards/5S.png5', 
     'Cards/2C.png2', 'Cards/3C.png3', 'Cards/4C.png4', 'Cards/5C.png5', 'Cards/6C.png6', 'Cards/7C.png7', 'Cards/8C.png8', 'Cards/9C.png9', 'Cards/10C.png10', 'Cards/JC.png11', 'Cards/QC.png12', 'Cards/KC.png13', 'Cards/AC.png1',
     'Cards/2D.png2', 'Cards/3D.png3', 'Cards/4D.png4', 'Cards/5D.png5', 'Cards/6D.png6', 'Cards/7D.png7', 'Cards/8D.png8', 'Cards/9D.png9', 'Cards/10D.png10', 'Cards/JD.png11', 'Cards/QD.png12', 'Cards/KD.png13', 'Cards/AD.png1'];
 // shuffle cards function
+var DeckClone = Deck;
+var difficulty;
+var cardCount = 0;
 function shuffle(Deck) {
     var j, x, i;
     for (i = Deck.length - 1; i > 0; i--) {
@@ -17,31 +20,35 @@ function shuffle(Deck) {
 //first function called by the only button on the home screan takes
 //difficulty value from the table and logically picks the coresponding game.
 function createNewGame() {
-    var difficulty = document.getElementById('fDifficulty').value;
+    difficulty = document.getElementById('fDifficulty').value;
     var body = document.getElementById('clear');
     body.innerHTML = '';
     if (difficulty == 1) { //Value 1 plays one full deck of cards any suit of the same value matches
         Easy();
+        cardCount = 0;
     }
     if (difficulty == 2) { //value 2 plays two full decks of cards any suit of the same value matches
         Easy();
         Easy();
+        cardCount = 0;
     }
     if (difficulty == 3) { //Value 3 plays 2 deck of cards, but only exact matches can be score points
         Medium(); // example: ace of spaces can only match with another ace of spades.
         Medium();
+        cardCount = 0;
     }
     if (difficulty == 4) { //Same as value 3, but double the decks!!
         Medium();
         Medium();
         Medium();
         Medium();
+        cardCount = 0;
     }
     gameMenue();
 }
 function gameMenue() {
     var body = document.getElementById('clear');
-    body.innerHTML += "<div><table style=\"border:2px solid blue\">\n        <thead>\n            <th>\n            Too hard? Change the difficulty\n            </th>\n        </thead>\n        \n        <tr>\n            <td>\n                Select Difficulty\n            </td>\n            <td><select name=\"Difficulty\" id=\"fDifficulty\">\n                <option type=\"number\" value=\"1\">Easy</option>\n                <option type=\"number\" value=\"2\">Medium</option>\n                <option type=\"number\" value=\"3\">Hard</option>\n                <option type=\"number\" value=\"4\">HardCore!</option>\n            </select></td>\n        </tr>\n        <tr><td>\n            <button class=\"button\" onclick=\"createNewGame()\">New Game</button>\n            </td>\n        </tr>\n        <tr>\n            <td>\n                <img src=\"Cards/aces.png\" alt=\"guess\" width=\"100px\">\n            </td>\n        </tr>\n        \n    </table></div>";
+    body.innerHTML += "<div><table style=\"border:2px solid blue\">\n        <thead>\n            <th>\n            Too hard? Change the difficulty\n            </th>\n        </thead>\n        \n        <tr>\n            <td>\n                Select Difficulty\n            </td>\n            <td><select name=\"Difficulty\" id=\"fDifficulty\">\n                <option type=\"number\" value=\"1\">Easy</option>\n                <option type=\"number\" value=\"2\">Medium</option>\n                <option type=\"number\" value=\"3\">Hard</option>\n                <option type=\"number\" value=\"4\">HardCore!</option>\n            </select></td>\n        </tr>\n        <tr><td>\n            <button class=\"button\" onclick=\"createNewGame()\">New Game</button>\n            </td>\n            <td><button class=\"button\" onclick=\"hide()\">Start</button></td>\n        </tr>\n        <tr>\n            <td>\n                <img src=\"Cards/aces.png\" alt=\"guess\" width=\"100px\">\n            </td>\n        </tr>\n        \n    </table></div>";
 }
 function Easy() {
     shuffle(Deck);
@@ -51,14 +58,15 @@ function Easy() {
     var i = Deck.length;
     for (var j = 0; Deck.length > j; j++) {
         --i;
+        cardCount++;
         var idx = Deck[j].lastIndexOf('g') + 1; // seperates the img string from the value string
         var img = Deck[j].slice(0, idx); // becomes the value
         var valueofCard = Deck[j].substring(idx, Deck[j].length);
         if (i % 13 != 0) {
-            row += "<img src=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + valueofCard + "\" type=\"string\"></img>";
+            row += "<img src='" + img + "' name=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + valueofCard + "\" type=\"string\" id=\"" + cardCount + "\" onclick=\"flip(" + cardCount + ")\"></img>";
         }
         if (i % 13 == 0) {
-            row += "<img src=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + valueofCard + "\" type=\"string\"></img></div><div>";
+            row += "<img src='" + img + "' name=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + valueofCard + "\" type=\"string\" id=\"" + cardCount + "\" onclick=\"flip(" + cardCount + ")\"></img></div><div>";
         }
     }
     body.innerHTML += row;
@@ -71,15 +79,35 @@ function Medium() {
     var i = Deck.length;
     for (var j = 0; Deck.length > j; j++) {
         --i;
+        cardCount++;
         var idx = Deck[j].lastIndexOf('g') + 1;
         var img = Deck[j].slice(0, idx);
         //var valueofCard = Deck[j].substring(idx,Deck[j].length)    
         if (i % 13 != 0) {
-            row += "<img src=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + img + "\" type=\"string\"></img>";
+            row += "<img src='" + img + "' name=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + img + "\" type=\"string\" id=\"" + cardCount + "\" onclick=\"flip(" + cardCount + ")\"></img>";
         }
         if (i % 13 == 0) {
-            row += "<img src=\"" + img + "\" alt=\"guess\" width=\"50px\" value=\"" + img + "\" type=\"string\"></img></div><div>";
+            row += "<img src=\"" + img + "\" name=\"" + img + "\"  alt=\"guess\" width=\"50px\" value=\"" + img + "\" type=\"string\" id=\"" + cardCount + " onclick=\"flip(" + cardCount + ")\"></img></div><div>";
         }
     }
     body.innerHTML += row;
+}
+function hide() {
+    var lookHere = document.getElementById('clear').outerHTML;
+    var replace = document.getElementById('clear');
+    replace.innerHTML = '';
+    for (var card = 0; DeckClone.length > card; card++) { //finds all the images and changes them to back of card
+        var idx = DeckClone[card].lastIndexOf('g') + 1;
+        var img = DeckClone[card].slice(0, idx);
+        var newbody = lookHere.replace(img, 'Cards/backofcard.png');
+        replace.innerHTML = newbody;
+        lookHere = newbody;
+    }
+}
+function flip(id) {
+    var cardtochange = document.getElementById(id).outerHTML;
+    var replace = document.getElementById(id).name;
+    var final = document.getElementById(id);
+    var idx = cardtochange.replace('Cards/backofcard.png', replace);
+    final.outerHTML = idx;
 }
--- match.ts
+++ match.ts
@@ -5,6 +5,9 @@ let Deck:string []= ['Cards/2S.png2', 'Cards/3S.png3','Cards/4S.png4','Cards/5S.
                      'Cards/2C.png2', 'Cards/3C.png3','Cards/4C.png4','Cards/5C.png5','Cards/6C.png6','Cards/7C.png7','Cards/8C.png8','Cards/9C.png9','Cards/10C.png10','Cards/JC.png11','Cards/QC.png12','Cards/KC.png13','Cards/AC.png1',
                      'Cards/2D.png2', 'Cards/3D.png3','Cards/4D.png4','Cards/5D.png5','Cards/6D.png6','Cards/7D.png7','Cards/8D.png8','Cards/9D.png9','Cards/10D.png10','Cards/JD.png11','Cards/QD.png12','Cards/KD.png13','Cards/AD.png1' ];
 // shuffle cards function
+let DeckClone:string[] = Deck;
+let difficulty:number;
+let cardCount: number = 0;
 function shuffle(Deck) {
     var j, x, i;
     for (i = Deck.length - 1; i > 0; i--) {
@@ -18,25 +21,29 @@ function shuffle(Deck) {
 //first function called by the only button on the home screan takes
 //difficulty value from the table and logically picks the coresponding game.
 function createNewGame(){
-    var difficulty = document.getElementById('fDifficulty').value;
+    difficulty = document.getElementById('fDifficulty').value;
     var body = document.getElementById('clear');
     body.innerHTML = '';
     if(difficulty ==1){//Value 1 plays one full deck of cards any suit of the same value matches
     Easy();
+    cardCount=0;
     }
     if(difficulty == 2){//value 2 plays two full decks of cards any suit of the same value matches
         Easy();
         Easy();
+        cardCount=0;
     }
     if(difficulty == 3){//Value 3 plays 2 deck of cards, but only exact matches can be score points
         Medium();       // example: ace of spaces can only match with another ace of spades.
         Medium();
+        cardCount=0;
     }
     if(difficulty == 4){//Same as value 3, but double the decks!!
         Medium();
         Medium();
         Medium();
         Medium();
+        cardCount = 0;
     }
     gameMenue();
 }    
@@ -67,6 +74,7 @@ function createNewGame(){
         <tr><td>
             <button class="button" onclick="createNewGame()">New Game</button>
             </td>
+            <td><button class="button" onclick="hide()">Start</button></td>
         </tr>
         <tr>
             <td>
@@ -85,14 +93,15 @@ function Easy(){
     let i = Deck.length;
     for(var j = 0; Deck.length>j; j++){
         --i;
+        cardCount++;
         var idx = Deck[j].lastIndexOf('g') + 1; // seperates the img string from the value string
         var img = Deck[j].slice(0,idx); // becomes the value
         var valueofCard = Deck[j].substring(idx,Deck[j].length)    
         if(i%13 != 0){ 
-            row +=`<img src="${img}" alt="guess" width="50px" value="${valueofCard}" type="string"></img>`;
+            row +=`<img src='${img}' name="${img}" alt="guess" width="50px" value="${valueofCard}" type="string" id="${cardCount}" onclick="flip(${cardCount})"></img>`;
             }
         if( i%13 == 0){
-            row += `<img src="${img}" alt="guess" width="50px" value="${valueofCard}" type="string"></img></div><div>`;
+            row += `<img src='${img}' name="${img}" alt="guess" width="50px" value="${valueofCard}" type="string" id="${cardCount}" onclick="flip(${cardCount})"></img></div><div>`;
         }
     }
     body.innerHTML+= row;
@@ -106,15 +115,42 @@ function Medium(){
     let i = Deck.length;
     for(var j = 0; Deck.length>j; j++){
         --i;
+        cardCount++;
         var idx = Deck[j].lastIndexOf('g') + 1;
         var img = Deck[j].slice(0,idx);
         //var valueofCard = Deck[j].substring(idx,Deck[j].length)    
         if(i%13 != 0){ 
-            row +=`<img src="${img}" alt="guess" width="50px" value="${img}" type="string"></img>`;
+            row +=`<img src='${img}' name="${img}" alt="guess" width="50px" value="${img}" type="string" id="${cardCount}" onclick="flip(${cardCount})"></img>`;
             }
         if( i%13 == 0){
-            row += `<img src="${img}" alt="guess" width="50px" value="${img}" type="string"></img></div><div>`;
+            row += `<img src="${img}" name="${img}"  alt="guess" width="50px" value="${img}" type="string" id="${cardCount} onclick="flip(${cardCount})"></img></div><div>`;
         }
     }
     body.innerHTML += row;
+}
+
+function hide(){ //hides all cards!
+    var lookHere = document.getElementById('clear').outerHTML;
+    var replace = document.getElementById('clear');
+    replace.innerHTML = '';
+
+
+    for(var card = 0;DeckClone.length>card; card++){//finds all the images and changes them to back of card
+        var idx = DeckClone[card].lastIndexOf('g') + 1;
+        var img = DeckClone[card].slice(0,idx);
+        var newbody = lookHere.replace(img,'Cards/backofcard.png');
+        replace.innerHTML= newbody;
+        lookHere = newbody;
+        
+    }
+    
+
+}
+
+function flip(id){
+    var cardtochange = document.getElementById(id).outerHTML;
+    var replace = document.getElementById(id).name;
+    var final = document.getElementById(id);
+    var idx = cardtochange.replace('Cards/backofcard.png', replace);
+    final.outerHTML = idx;
 }
